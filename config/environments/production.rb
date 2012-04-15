c0406e5078250afc5f2a6db9a344a8e9d340b54b
David <dev.david.andre@gmail.com>
2012-04-15 18:41:16 +0200

    testing for asset precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ RubyWorkshop::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
-  config.assets.precompile += %w( application.css  )
+  config.assets.compile = false
+  config.assets.precompile += %w( application.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
